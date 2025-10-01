61b6bd46974ea68bc2c71864699efd913c23db28
Day Quiroga <dbquiroga.day@gmail.com>
2025-10-01 00:01:54 +0000

    cambio msje sucess login

--- pages/Page1/jsobjects/JSObject1/JSObject1.js
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -1,12 +1,5 @@
 export default {
-	myVar1: [],
-	myVar2: {},
-	myFun1 () {
+	btn_signInonClick () {
 		//	write code here
-		//	this.myVar1 = [1,2,3]
-	},
-	async myFun2 () {
-		//	use async-await or promises
-		//	await storeValue('varName', 'hello world')
 	}
 }
