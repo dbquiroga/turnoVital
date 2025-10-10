63414b2bcf0525c806b7b394db46895e05068778
Day Quiroga <dbquiroga.day@gmail.com>
2025-10-10 00:55:59 +0000

    consolo log al guardar profesional

--- pages/Registrar Profesional/jsobjects/JSProfesional/JSProfesional.js
+++ pages/Registrar Profesional/jsobjects/JSProfesional/JSProfesional.js
@@ -1,18 +1,24 @@
 export default {
-  async guardarProfesional() {
-    try {
-      await crearDomicilio.run();
-      storeValue("domicilio_id", crearDomicilio.data[0].id);
-   
-      await crearPersona.run();
-      storeValue("persona_id", crearPersona.data[0].id);
+	async guardarProfesional() {
+		try {
+			await crearDomicilio.run();
+			storeValue("domicilio_id", crearDomicilio.data[0].id);
+			console.log(crearDomicilio.data);
 
-      await crearProfesional.run();
 
-      showAlert("Profesional registrado con éxito ✅", "success");
-    } catch (e) {
-      showAlert("Error al registrar profesional ❌", "error");
-      console.log(e);
-    }
-  }
+			await crearPersona.run();
+			storeValue("persona_id", crearPersona.data[0].id);
+			console.log(crearPersona.data);
+
+
+			await crearProfesional.run();
+			console.log(crearProfesional.data);
+
+
+			showAlert("Profesional registrado con éxito ✅", "success");
+		} catch (e) {
+			showAlert("Error al registrar profesional ❌", "error");
+			console.log(e);
+		}
+	}
 }
